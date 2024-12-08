ca08776522a20b07e39cf5a24f6190ba78382d12
bbrodo <15292@coderacademy.edu.au>
2024-12-08 16:43:42 +1100

    test

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-var Id = new mongoose.Types.ObjectId();
 const UserSchema = new mongoose.Schema({
-  _id: Id,
   username: {
     type: String,
     required: [true, 'Username is required'],
