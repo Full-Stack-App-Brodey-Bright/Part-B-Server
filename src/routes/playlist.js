640cc14f0a1e10b6d91f32f0eddd1d537a944111
bbrodo <15292@coderacademy.edu.au>
2024-12-14 13:47:22 +1100

    rtyui

--- src/routes/playlist.js
+++ src/routes/playlist.js
@@ -79,10 +79,10 @@ router.get('/', validateJWT, async (req, res) => {
 
     // Fetch playlists
     let playlists = null
-    if (!id && !searchQuery) {
+    if (!id && searchQuery === 'false') {
       // if there is no id in url show all playlists
       playlists = await Playlist.find({$or: [{creator: user2}, {likes: user2._id}, query]});
-    }else if (searchQuery === 'true') {
+    }else if (!searchQuery === 'false') {
       playlists = await Playlist.aggregate([
         {
           $search: {
